03d92430418c54350f67d5ddd7ce43706de92207
Darshan Yogesh Patil <86610850+Darshan172003@users.noreply.github.com>
2023-12-13 07:28:40 +0000

    variables

--- 2_JS/01_variables.js
+++ 2_JS/01_variables.js
@@ -1,4 +1,4 @@
-const AC_no = 123456789;
+const AC_no = 123456789; // const is used to declare a variable which can't be changed once it is assigned a value
 let AC_Email = "abc.gmail.com";
 
 var AC_password = "password123"; // var is not used in javascript because it is a global variable and can be accessed from anywhere in the program which is not a good practice
@@ -6,6 +6,7 @@ var AC_password = "password123"; // var is not used in javascript because it is 
 account_balance = 1000000; // if we don't use let or var then it is considered as a global variable and can be accessed from anywhere in the program which is not a good practice
 
 let account_city = "Bangalore"; // let is used to declare a variable which can be accessed only within the block of code where it is declared and not outside the block of code
+
 let accoutn_state; // variable declared but not assigned any value to it so it is undefined
 
 // AC_no = 987654321; // Error: Assignment to constant variable. not allowed to change the value of constant variable
