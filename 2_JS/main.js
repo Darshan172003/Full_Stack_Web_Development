9590f26df74fb45d7db8000f6dc85124b5035e99
Darshan Yogesh Patil <86610850+Darshan172003@users.noreply.github.com>
2023-12-13 04:50:35 +0000

    stage 1

--- 2_JS/main.js
+++ 2_JS/main.js
@@ -1,24 +1,2 @@
-// Define the size of the triangle
-const size = 5;
-
-// Loop through each row of the triangle
-for (let i = 1; i <= size; i++) {
-    // Create a string to represent the current row
-    let row = '';
-
-    // Add spaces to the beginning of the row to center the triangle
-    for (let j = 1; j <= size - i; j++) {
-        row += ' ';
-    }
-
-    // Add asterisks to the row to create the triangle
-    for (let k = 1; k <= i; k++) {
-        row += '* ';
-    }
-
-    // Log the row to the console
-    console.log(row);
-}
-
 console.log("Namaste!")
 console.log("Let's Go!")
