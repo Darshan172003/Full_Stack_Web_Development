5a554d44e795eeeb77c23e6ddb4444e1638f8cef
Darshan Yogesh Patil <86610850+Darshan172003@users.noreply.github.com>
2023-12-19 13:33:51 +0000

    conditional expression

---
+++ 2_JS/05_Conditional_expression.js
@@ -0,0 +1,12 @@
+
+let a = prompt("Enter Your age: "); // by default prompt() returns string
+
+
+a = Number.parseInt(a); // converting string to number using parseInt() function of Number class
+
+// conditional expression
+
+if (a >=18) {
+    console.log("You are eligible to vote.");
+}
+
