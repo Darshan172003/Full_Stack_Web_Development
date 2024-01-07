b5f4ed48f0c53646a3cd2154af0ae17e1ec2ea13
Darshan Yogesh Patil <86610850+Darshan172003@users.noreply.github.com>
2024-01-07 06:04:22 +0000

    String Methods

---
+++ 2_JS/08_Strings.js
@@ -0,0 +1,13 @@
+// String 
+
+let F_name = "John";
+let Age = 23;
+
+// console.log(F_name + " is " + Age + " years old."); // concatenation // old way to do it
+
+console.log(`${F_name} is ${Age} years old.`); // string interpolation // new way to do it
+
+const newString = "Hello World";
+
+console.log(newString.length); // length of the string  
+
