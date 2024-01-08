133650b5e1ea36c37d4fb5dd659a3978aa48be44
Darshan Yogesh Patil <86610850+Darshan172003@users.noreply.github.com>
2024-01-08 04:09:52 +0000

    String Methods

--- 2_JS/08_Strings.js
+++ 2_JS/08_Strings.js
@@ -8,17 +8,21 @@ let Age = 23;
 console.log(`${F_name} is ${Age} years old.`); // string interpolation // new way to do it
 
 const newString = "Hello World"; // string literal
-const GameZone = new String("Namaste Bharat"); // string object
+const newStringOne = new String("Namaste Bharat"); // string object
 
 console.log(newString.length); // length of the string
 
 console.log(newString.toLowerCase()); // convert to lower case , the original string is not changed because strings are immutable in JS and we have to store it in a new variable
 
-console.log(GameZone.indexOf("Bharat")); // index of the string
+console.log(newStringOne.indexOf("Bharat")); // index of the string
 
-console.log(GameZone.__proto__); // prototype of the string object used to access the methods of the string object but not recommended to use it because it is not a good practice to use it
+console.log(newStringOne.__proto__); // prototype of the string object used to access the methods of the string object but not recommended to use it because it is not a good practice to use it
 
-console.log(GameZone.charAt(2)); // character at the index 2
+console.log(newStringOne.charAt(2)); // character at the index 2
 
-console.log(`An index of ${GameZone} at 2 is ${GameZone.at(2)}`); // character at the index 2
+console.log(`An index of ${newStringOne} at 2 is ${newStringOne.at(2)}`); // character at the index 2
+
+console.log(`ASCII code of index 4 is ${newStringOne.codePointAt(4)}`); // returns ASCII code of index 4 
+
+console.log(`${newString.concat(" and ",newStringOne)}`); // concatenates two strings and returns a new string
 
