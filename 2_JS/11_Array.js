e9a53494706e69ed614ff3aa2d6ea5db166ccd02
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2024-05-20 20:59:19 +0530

    Array

--- 2_JS/11_Array.js
+++ 2_JS/11_Array.js
@@ -1,7 +1,7 @@
 // Array 
 
 const myArray = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const myheros = ['spider man ', 'iron man', 'hulk']
+
 
 console.log(myArray);
 
@@ -33,4 +33,32 @@ console.log(Array1.splice(2, 5)); // remove the elements from the given start in
 
 
 console.log(`Original array is ${Array1}`); // original array
+
+const Marvel = ['spider man ', 'iron man', 'hulk']
+const DC = ['batman', 'superman', 'Black Adam']
+
+const SuperHeros = Marvel.concat(DC); // Concatenate two arrays and return a new array
+
+console.log(SuperHeros);
+
+const SuperHeros1 = [...Marvel, ...DC]; // Concatenate two arrays using spread operator and return a new array 
+
+console.log(SuperHeros1);
+
+
+const anotherArray = [1,2,3,4,5,6,7,8,9,10,[11,12,13,14,15],[1,2,3[23,21,21]]];
+
+console.log(anotherArray.flat(2)); // flat the array to the given depth and return a new array you can also use Infinity to flat the array to the infinite depth
+
+console.log(Array.isArray("Namaste")); // check if the given element is an array or not
+
+console.log(Array.from("Namaste")); // convert the given element to an array
+
+console.log(Array.from({Name: "Darshan"})); // convert the elements of the object to an array empty array will be returned
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); // convert the given elements to an array
 
